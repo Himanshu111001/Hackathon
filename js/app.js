bcc39fd079a318a955d0040d0f765107c325bdef
Himanshu <shishodia1977@gmail.com>
2021-09-04 20:14:08 +0530

    navigation bar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("change-bar");
+
+    document.getElementById("nav").classList.toggle("change-nav");
+
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
